e479ae34ff724c55ebc6ad401b22f227d012988f
whitesuger0912 <watermelons6701@gmail.com>
2020-03-07 15:46:43 +0800

    main.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+bot = commands.bot(commands_prefix= "[")
+
+@bot.event 
+async def on_ready():
+     print("bot is online")
+     
+bot.run(Njg1NjkyMzg5NzQyMjE1MzEz.XmMkjA.f6-Ec7YfPo4eYQWUS40gwsNJEaA)
